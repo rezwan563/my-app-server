ce98d8cdbe5093f06809af46036a044f16d7a7a3
Rezwan <t.rewan17@gmail.com>
2024-04-20 22:40:39 +0600

    added multer to handle file at /url endpoiont

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const bodyParser = require("body-parser");
 const fs = require("fs");
+const multer = require("multer");
+const upload = multer({ dest: "./uploads" });
 const port = 3000;
 
 app.use(cors());
-app.use(bodyParser.json());
+app.use(upload.any());
 
 let seriolNo = 0;
 app.post("/", (req, res) => {
@@ -21,14 +22,16 @@ app.post("/", (req, res) => {
   res.json({ message: message });
 });
 
-app.get(/\/pqr|\/def/, (req, res) => {
-  console.log(req.path)
-  const apiPath = req.path;
-  const apiEndPoint = apiPath.replace('/','');
-  console.log('endpoint - ', apiEndPoint)
+app.post("/url", (req, res) => {
   const message = "new route yay!";
-  console.log(message);
-  res.send({status: 'success', message: 'you\'ve reached your destination'});
+  try {
+    console.log("req.body\n", req.body);
+    console.log("req.file\n", req.files);
+  } catch (error) {
+    console.log(error);
+  }
+
+  res.send({ status: "success", message: "you've reached your destination" });
 });
 app.listen(port, () => {
   console.log(`server listening on ${port}`);
