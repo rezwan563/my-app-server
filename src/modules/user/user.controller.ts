a27657ac97e7122495ccfc79ff1994607434c96b
Rezwan <t.rewan17@gmail.com>
2024-04-30 22:04:31 +0600

    Added bcrypt to hash password

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,15 +1,16 @@
 import { Request, Response } from "express";
 import { createUserService } from "./user.service";
+import bcrypt from "bcrypt";
 
 export const handleSignup = async (req: Request, res: Response) => {
   try {
     console.log('sign up handler')
     const { email, password } = req.body;
-    const createUser = await createUserService(email, password)
+    const hashedPassword = await bcrypt.hashSync(password, 10)
+    const createUser = await createUserService(email, hashedPassword)
     const responseMessage = {
       status: 'success',
       message: 'user created',
-      result: createUser
     }
     res.send(responseMessage);
   } catch (error) {
